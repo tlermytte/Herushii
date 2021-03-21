552e2a98b444dd67eed8e0bd957c69cd72bfef6d
Thiabut Lermytte <thibaut.lermytte@autodicar.fr>
2021-03-21 22:31:41 +0100

    [MOD] title

--- components/title.tsx
+++ components/title.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './title.module.scss';
 
 export default function Title() {
-  return <h1 className={styles.title}>Bonjour</h1>;
+  return <h1 className={styles.title}>Salut</h1>;
 }
