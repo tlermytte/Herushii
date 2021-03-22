a75879639ccd425c83497c5679eb7f4d49de0402
Thiabut Lermytte <thibaut.lermytte@autodicar.fr>
2021-03-22 11:46:44 +0100

    [ADD] some

--- components/CTA/cta.tsx
+++ components/CTA/cta.tsx
@@ -2,41 +2,27 @@ import dynammic from 'next/dynamic';
 
 import style from './cta.module.scss';
 
-const Link = dynammic(() => import('next/link'));
 const Logo = dynammic(() => import('../Logo/logo'));
 
 interface Props {
   text: string;
-  ctaType: 'button' | 'link';
+  type: 'social' | 'e-mail' | 'button';
   logo?: boolean;
   buttonType?: 'button' | 'submit';
-  href?: string;
   onClick?: () => void;
 }
 
 export default function CTA({
   text,
-  ctaType,
+  type,
   logo,
   buttonType,
-  href,
   onClick,
 }: Props): JSX.Element {
   return (
-    <div className={style.cta}>
-      {ctaType === 'link' ? (
-        <Link href={href}>
-          <a href={href}>
-            {text}
-            {logo && <Logo />}
-          </a>
-        </Link>
-      ) : (
-        <button onClick={onClick} type={buttonType}>
-          {text}
-          {logo && <Logo />}
-        </button>
-      )}
-    </div>
+    <button className={style.cta} onClick={onClick} type={buttonType}>
+      {text}
+      {logo && <Logo />}
+    </button>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
 import Head from 'next/head';
 import Header from '../components/Header/header';
+import CTA from '../components/CTA/cta';
+
+import style from '../styles/index.module.scss';
 
 export default function Home() {
   return (
-    <>
+    <div className={style.container}>
       <Head>
         <title>Herushii</title>
       </Head>
       <Header />
-    </>
+      <main>
+        <section className={style.presentation}>
+          <div id='content'>
+            <h1>Be Healthier, Easier !</h1>
+            <p>
+              Thanks to Heru, the AI that helps you in your diet, you will have
+              all the keys in hand to adapt your diet to your needs!
+            </p>
+            <CTA text={`How does it work?`} type='e-mail' />
+          </div>
+          <object data='/sapiens.svg' type='image/svg+xml'></object>
+        </section>
+        <section className={style.features}></section>
+      </main>
+    </div>
   );
 }
